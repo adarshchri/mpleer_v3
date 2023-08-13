a65768a5b6d4dba942a07ed2d28aa7a1625f92d3
adarshchri <prodevadarsh@outlook.com>
2023-08-14 00:57:12 +0530

    feat: Server Update

--- movie/config.py
+++ movie/config.py
@@ -1,12 +1,12 @@
 movie_servers_config = {
     "vidsrc": "vidsrc.me/embed/movie?tmdb={0}",
     "pogolinks": "pogolinks.lol/se_player.php?video_id={0}",
+    "smashystream": "embed.smashystream.com/playere.php?tmdb={0}",
     "superembed": "multiembed.mov/directstream.php?video_id={0}&tmdb=1",
     "remotestream": " remotestre.am/e/?tmdb={0}",
     "movieapi": "moviesapi.club/movie/{0}",
     "2embed": "www.2embed.cc/embed/{0}",
     "autoembed": "autoembed.to/movie/tmdb/{0}",
-    "smashystream": "embed.smashystream.com/playere.php?tmdb={0}",
     "gdrive": "mostream.me/embed.php?tmdb={0}"
 }
 
